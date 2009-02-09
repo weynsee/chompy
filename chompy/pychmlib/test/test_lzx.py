d5ae6129d1b4c0a6476364914d969db94e2f81ef
Wayne See <weynsee@gmail.com>
2009-02-09 21:32:59 +0800

    initial test files for lzx algorithm

--- chompy/pychmlib/test/test_lzx.py
+++ chompy/pychmlib/test/test_lzx.py
@@ -1,21 +1,18 @@
 import unittest
-import struct
 from pychmlib.lzx import create_lzx_block
 
 class LZXTest(unittest.TestCase):
-    
-    def setUp(self):
-        fmt = 'b '* 67
-        data = struct.pack(fmt,16,
-                16, 0, 0, 0, 0, 2, 0, 0, 19, 0, 0, 49, 0, 62, 42, -93, -124,
-                -96, -23, -67, -88, 33, -18, 94, 94, -117, 11, -9, -34, 111,
-                -68, 19, 34, 72, -22, -63, -117, 79, 31, 41, -86, -96, 20, 82,
-                -91, 21, 85, -110, 72, 18, -91, -86, 84, 85, 82, 70, 21, -120,
-                0, 104, 68, 0, -83, 0, 0, 29)
-        self.block = create_lzx_block(1, 65536, data, 32768)
         
-    def test_block_no(self):
-        self.assertEquals(1,self.block.block_no)
+    def test_lzx_1(self):
+        data = open_file("lzx_files/lzx_1")
+        block = create_lzx_block(block_no=50, window=65536, bytes=data, block_length=32768)
+
+def open_file(filename):
+    f = open(filename, "rb")
+    try:
+        return f.read()
+    finally:
+        f.close()
 
 
 if __name__ == "__main__":
